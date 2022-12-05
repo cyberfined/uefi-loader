baa8b6f9b4a28181e3f883d97d1e8008cb824118
cyberfined <cyberfined@protonmail.com>
2022-12-05 21:08:32 +0300

    Fix run-qemu

--- image/run-qemu.sh
+++ image/run-qemu.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-qemu-system-x86_64 -enable-kvm -cpu host \
+qemu-system-x86_64 \
     -drive if=pflash,format=raw,unit=0,file=./OVMF_CODE.fd,readonly=on \
     -drive if=pflash,format=raw,unit=1,file=./OVMF_VARS.fd -net none -drive if=ide,format=raw,file=uefi.img
